4134f2029ee384e030bb9c93d49147bd5c0d40cf
lordever <lordever@mail.ru>
2026-01-04 15:59:30 +0500

    valid anagram has been resolved

---
+++ string/valid-anagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const newS = s.split("").sort().join("");
+    const newT = t.split("").sort().join("");
+
+    return newS === newT;
+};
+
+console.log(isAnagram('anagram', 'nagaram')) // true
+console.log(isAnagram('rat', 'car')) // false
